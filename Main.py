494fb0b93857d0dcf51ca741f9a7971d76a4e62f
Emilio Gordillo <gor18062@uvg.edu.gt>
2024-01-22 00:26:05 -0600

    Cambios en la logica del programa
    Se reestructuro el programa para mejorar la velocidad con la que se descargan y crean los archivos

--- Main.py
+++ Main.py
@@ -1,44 +1,80 @@
 from funciones import *
 
-# Configuración directorio WEB
-base_url = 'https://www.amm.org.gt/pdfs2/programas_despacho/'
-dir1_url = '01_PROGRAMAS_DE_DESPACHO_DIARIO'
-dir2_url = '01_PROGRAMAS_DE_DESPACHO_DIARIO'
-
-# Configura la localización en español
-locale.setlocale(locale.LC_TIME, 'es_ES.utf8')
+# Fechas para la muestra
+fecha_inicial = datetime(2023, 1, 1)
+fecha_final = datetime(2023, 6, 30)
 
 
 if __name__ == "__main__":
-    # Define las fechas iniciales y finales
-    fecha_inicial = datetime(2023, 1, 1)
-    fecha_final = datetime(2023, 6, 30)
+    base_url = 'https://www.amm.org.gt/pdfs2/programas_despacho/'
+    dir1_url = '01_PROGRAMAS_DE_DESPACHO_DIARIO'
+    dir2_url = '01_PROGRAMAS_DE_DESPACHO_DIARIO'
 
-    # Descargar archivos para cada día en el rango especificado
+    locale.setlocale(locale.LC_TIME, 'es_ES.utf8')
+
     fecha_actual = fecha_inicial
 
-    df_calculos = pd.DataFrame()
+    # Define a list to store downloaded file paths
+    downloaded_files = []
+
     while fecha_actual <= fecha_final:
-        # Formatea la fecha actual
+        
         fecha_str_doc = fecha_actual.strftime("%d%m%Y")
         fecha_str_dir = fecha_actual.strftime("%m_%B").upper()
         year_str = fecha_actual.strftime("%Y")
-
+        
         url_dir = f'{dir1_url}/{year_str}/{dir2_url}/{fecha_str_dir}/WEB{fecha_str_doc}.xlsx'
-
-        # Construye la URL
         url_descarga = urljoin(base_url, url_dir)
-
-        # Construye el directorio
         os.makedirs(os.path.dirname(url_dir), exist_ok=True)
-        nombre_local_excel = url_dir
 
-        # Descargar el archivo descomentando la siguiente línea cuando estés listo
-        df_demandas = descargar_excel(url_descarga, nombre_local_excel, fecha_actual.date())
+        # Descargar archivo actual
+        try:
+            descargar_excel(url_descarga, url_dir)
+            downloaded_files.append(url_dir)
 
-        # Agrega un retraso entre solicitudes para simular comportamiento humano
-        # time.sleep(1)  # Ajusta el valor según sea necesario
+        except requests.exceptions.RequestException as err:
+            manejar_error(err)
 
+        fecha_actual += timedelta(days=1)
+
+    # Variables temporales
+    df_calculos = pd.DataFrame()
+    fecha_actual = fecha_inicial
+    sheet_name = 'LDM'
+    
+    
+    # Process the data after downloading all files
+    for file_path in downloaded_files:
+        
+        fecha_str_dir = fecha_actual.strftime("%m_%B").upper()
+        year_str = fecha_actual.strftime("%Y")
+
+        # Abrir documuento actual
+        wb = load_workbook(file_path)
+
+        if sheet_name not in wb.sheetnames:
+            raise ValueError(f'No se encontró la hoja con el nombre "{sheet_name}" en el archivo Excel.')
+
+        # Seleccionar la hoja LDM
+        sheet = wb[sheet_name]
+
+        # Extraer las 3 tablas de cada archivo diario
+        df_dict = extraer_tablas(sheet, fecha_actual)
+
+        # Dataframes Temporales
+        df_demandas = pd.DataFrame()
+        
+        # Concatenacion de todos los dataframe del dia
+        for table_name, df in df_dict.items():
+            df_demandas = pd.concat([df_demandas, df], ignore_index=True)
+        
+        # Reordenar el Dataframe
+        df_demandas.columns = df.columns.str.strip()
+        df_demandas = df_demandas.rename(columns={'Costo en US$/MWH': 'Costo'})
+        df_demandas = df_demandas[['fecha_hora', 'Nemo', 'Planta Generadora', 'Potencia Disponible', 'Costo', 'FPNE', 'Banda']]
+        df_demandas = df_demandas.sort_values(by='fecha_hora')
+
+        # Leer archivos POE y Generacion
         df_poe = pd.read_csv('POE.csv')
         df_poe['fecha_hora'] = pd.to_datetime(df_poe['fecha_hora'])
 
@@ -53,18 +89,28 @@ if __name__ == "__main__":
         df_demandas = pd.merge(df_demandas, df_poe, on='fecha_hora')
         df_demandas = pd.merge(df_demandas, df_generacion, on='fecha_hora')
 
+        # Calculos de Indicador
         df_demandas['Indicador'] = (df_demandas['POE'] > df_demandas['Costo']).astype(int)
+
+        # Calculos de Liquidaciones
         df_demandas['Liquidacion POE'] = df_demandas['POE']*df_demandas['generacion']
         df_demandas['Liquidacion CVG'] = df_demandas['Costo']*df_demandas['generacion']
+
+        # Calculos de Agentes
         df_demandas['Agente A'] = df_demandas['Indicador']*(df_demandas['Liquidacion POE'] - df_demandas['Liquidacion CVG'])
         df_demandas['Agente B'] = (df_demandas['Indicador']-1)*(df_demandas['Liquidacion POE'] - df_demandas['Liquidacion CVG'])
 
-        nombre_archivo_corrected = nombre_local_excel.replace(".xlsx", "")
-        df_demandas.to_csv(f"{nombre_archivo_corrected}_DEMANDAS.csv", index=False, sep=getListSeparator())
-        print(f'Archivo "{nombre_archivo_corrected}_DEMANDAS.csv" guardado exitosamente.')
-        print("----------------------------------")
+        nombre_archivo_corrected = file_path.split('/')[-1].split('.')[0]
+        local_dir = f'{dir1_url}/{year_str}/{dir2_url}/{fecha_str_dir}/{nombre_archivo_corrected}'
+        
+        df_demandas.to_csv(f"{local_dir}_DEMANDAS.csv", index=False, sep=getListSeparator())
+        print(f'Archivo "{local_dir}_DEMANDAS.csv" guardado exitosamente.')
+        print("----------------------------------\n")
 
+        # Concatenar el dataframe de demandas del dia con el resto
         df_calculos = pd.concat([df_calculos, df_demandas], ignore_index=True)
-        fecha_actual += timedelta(days=1)
 
+        fecha_actual += timedelta(days=1)
+        
+    # Save the final result to a CSV file
     df_calculos.to_csv(f"RESUMEN_DEMANDAS.csv", index=False, sep=getListSeparator())
--- funciones.py
+++ funciones.py
@@ -1,5 +1,4 @@
-import locale
-import os
+# Librerias
 from datetime import datetime, timedelta
 from io import BytesIO
 import pandas as pd
@@ -7,13 +6,15 @@ import requests
 import time
 import unidecode
 import winreg
+import os
+import locale
 
 from openpyxl import load_workbook
-from openpyxl.utils import get_column_interval
 from openpyxl.utils.cell import coordinate_from_string as cfs
 from openpyxl.utils.cell import get_column_letter as gcl
 from urllib.parse import urljoin
 
+# Funciones 
 def getListSeparator():
     '''Retrieves the Windows list separator character from the registry'''
     aReg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
@@ -26,7 +27,6 @@ def manejar_error(excepcion):
 def convert_rng_to_df(tlc, l_col, l_row, sheet):
     first_col = cfs(tlc)[0]
     first_row = cfs(tlc)[1]
-
     rng = f"{first_col}{first_row+1}:{l_col}{l_row}"
 
     data_rows = []
@@ -36,37 +36,41 @@ def convert_rng_to_df(tlc, l_col, l_row, sheet):
     return pd.DataFrame(data_rows[2:], columns=data_rows[0])
 
 def generar_dataframe(fecha_inicio, fecha_final):
-    # Convertir las cadenas de fecha a objetos datetime
     fecha_inicio = datetime.strptime(fecha_inicio, '%Y-%m-%d %H:%M:%S')
     fecha_final = datetime.strptime(fecha_final, '%Y-%m-%d %H:%M:%S')
 
-    # Crear un rango de fechas intermedias con intervalo de 1 hora
-    delta = timedelta(hours=1)  # Intervalo de 1 hora
+    delta = timedelta(hours=1)
     fechas_intermedias = [fecha_inicio + i * delta for i in range(int((fecha_final - fecha_inicio) / delta)+1)]
 
     return pd.DataFrame({'fecha_hora': fechas_intermedias})
 
-def descargar_excel(url, nombre_archivo, fecha_actual):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Lanza una excepción para códigos de estado no exitosos
+def descargar_excel(url_descarga, url_dir):
+    response = requests.get(url_descarga)
+    response.raise_for_status()
 
-        # Carga el archivo Excel con openpyxl
-        wb = load_workbook(BytesIO(response.content))
+    with open(url_dir, 'wb') as file:
+        file.write(response.content)
         
-        # Especifica la hoja con el nombre "LDM"
-        sheet_name = 'LDM'
-        if sheet_name not in wb.sheetnames:
-            raise ValueError(f'No se encontró la hoja con el nombre "{sheet_name}" en el archivo Excel.')
+    print(f'Archivo "{url_dir}" guardado exitosamente.')
+    print("----------------------------------\n")
 
-        sheet = wb[sheet_name]
+def extraer_tablas(sheet, fecha_actual):
+    # Variables
+    banda_mapping = {
+                     "DEMANDA MINIMA": (("00:00:00", "05:00:00"), ("22:00:00", "23:00:00")),
+                     "DEMANDA MEDIA": (("06:00:00", "17:00:00"),),
+                     "DEMANDA MAXIMA": (("18:00:00", "21:00:00"),)
+                    }
+    section_headers = ['DEMANDA MÍNIMA', 'DEMANDA MEDIA', 'DEMANDA MÁXIMA']
 
-        section_headers = ['DEMANDA MÍNIMA', 'DEMANDA MEDIA', 'DEMANDA MÁXIMA']
+    # Variables temporales
+    last_col = ''
+    last_row = ''
+    df_dict = {}  # Dictionary to hold the dataframes
+    
+    fecha_actual_normalized = fecha_actual.date()
 
-        last_col = ''
-        last_row = ''
-        df_dict = {}  # Dictionary to hold the dataframes
-        for cell in sheet['A']:  # Looping Column A only
+    for cell in sheet['A']:  # Looping Column A only
             if cell.value in section_headers:
                 tblname = cell.value  # Header of the Data Set found
                 tlc = cell.coordinate  # Top Left Cell of the range
@@ -89,14 +93,6 @@ def descargar_excel(url, nombre_archivo, fecha_actual):
 
                 tblname_normalized = unidecode.unidecode(tblname)
 
-                banda_mapping = {
-                     "DEMANDA MINIMA": (("00:00:00", "05:00:00"), ("22:00:00", "23:00:00")),
-                     "DEMANDA MEDIA": (("06:00:00", "17:00:00"),),
-                     "DEMANDA MAXIMA": (("18:00:00", "21:00:00"),)
-                    }
-
-                tblname_normalized = unidecode.unidecode(tblname)
-
                 # Crear un DataFrame vacío
                 df_resultante = pd.DataFrame()
                 if tblname_normalized in banda_mapping:
@@ -104,40 +100,10 @@ def descargar_excel(url, nombre_archivo, fecha_actual):
 
                     for hora_inicio, hora_final in horas_intervalos:
                         # Generar el DataFrame
-                        df_temporal = generar_dataframe(f'{fecha_actual} {hora_inicio}', f'{fecha_actual} {hora_final}')
+                        df_temporal = generar_dataframe(f'{fecha_actual_normalized} {hora_inicio}', f'{fecha_actual_normalized} {hora_final}')
                         df_resultante = pd.concat([df_resultante, df_temporal], ignore_index=True)
 
                     df_resultante["Banda"] = tblname_normalized
                     df_dict[tblname] = pd.merge(df_dict[tblname], df_resultante, on='Banda')
-                else:
-                    pass
-
-        wb.save(f"{nombre_archivo}")
-        print("\n")
-        print(f'Archivo "{nombre_archivo}" guardado exitosamente.')
-        ### Print the DataFrames
-        
-        df_resultado = pd.DataFrame()
-        for table_name, df in df_dict.items():
-            df_resultado = pd.concat([df_resultado, df], ignore_index=True)
-
-        df_resultado.columns = df.columns.str.strip()
-        df_resultado = df_resultado.rename(columns={'Costo en US$/MWH': 'Costo'})
-        df_resultado = df_resultado[['fecha_hora', 'Nemo', 'Planta Generadora', 'Potencia Disponible', 'Costo', 'FPNE', 'Banda']]
-        df_resultado = df_resultado.sort_values(by='fecha_hora')
-
-        return df_resultado
-            
-    except requests.exceptions.HTTPError as errh:
-        manejar_error(errh)
-    except requests.exceptions.ConnectionError as errc:
-        manejar_error(errc)
-    except requests.exceptions.Timeout as errt:
-        manejar_error(errt)
-    except requests.exceptions.RequestException as err:
-        manejar_error(err)
-    except pd.errors.EmptyDataError:
-        print(f'No se encontró la hoja con el nombre "LDM" en el archivo Excel.')
-    except Exception as e:
-        manejar_error(e)
-    
+                    
+    return df_dict
